583bd479ca5218d4737b92e82a49345e38accfe6
Daniel <isral.tudela712.96@gmail.com>
2025-07-07 11:43:12 -0400

    borre un archivo que no tenia nada

--- src/scripts/utils.js
+++
@@ -1,5 +0,0 @@
-/**
- * Módulo de funciones utilitarias.
- * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
- * desde el archivo principal para realizar varias operaciones.
- */
